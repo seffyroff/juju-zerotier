69bd09e6a2f2393c888777a63c6be1167b151ae3
Phil Merricks <seffyroff@gmail.com>
2019-05-05 16:15:30 -0700

    fix for linting errors

--- reactive/zerotier.py
+++ reactive/zerotier.py
@@ -1,15 +1,19 @@
 from charms.reactive import when, when_not, set_flag, when_all
+from charmhelpers.core.hookenv import status_set, config
 from subprocess import call
 
 
 @when_not('zerotier.installed')
 def install_zerotier():
     status_set('maintenance', 'Installing Zerotier Client')
-    
-    cmd_install = \
-        ('curl -s "https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg"'
-            '| gpg --import && if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi'.format())
-    call(cmd_install.split())
+
+    # cmd_install = ('curl -s https://install.zerotier.com/ | sudo bash')
+
+    cmd_install = ('curl -s \'https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg\' '
+                   '| gpg --import && if z=$(curl -s \'https://install.zerotier.com/\' | gpg); then echo \"$z\" | sudo bash; fi')
+
+    call(cmd_install)
+
     set_flag('zerotier.installed')
 
 @when('zerotier.installed')
@@ -24,4 +28,5 @@ def zt_join_network():
     set_flag('zerotier.network.joined')
 
 @when_all('zerotier.installed', 'zerotier.network.joined')
+def set_status_active():
     status_set('active', 'Connected to network {}'.format(config('zerotier-network-id')))
