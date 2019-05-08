c5d29f8be31434e32d0128ff659d6d1a1002d476
Phil Merricks <seffyroff@gmail.com>
2019-05-08 01:17:02 -0700

    cleanup

--- reactive/zerotier.py
+++ reactive/zerotier.py
@@ -1,28 +1,27 @@
 from charms.reactive import when, when_not, set_flag, when_all
-from charmhelpers.core.hookenv import status_set, config
+from charmhelpers.core.hookenv import status_set, config, application_version_set
+from charmhelpers.fetch import get_upstream_version
 import subprocess
-
-# zerotier_network_id = config('zerotier-network-id')
+import json
 
 @when_not('zerotier.installed')
 def install_zerotier():
     status_set('maintenance', 'Installing Zerotier Client')
 
-    install_ctxt = {'install_url': ("https://install.zerotier.com/"),
-                    'install_exec': ('sudo bash')}
+    install_ctxt = {'install_url': ("https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg"),
+                    'install_exec': ("gpg --import && if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo \"$z\" | sudo bash; fi")}
     cmd_install = ('curl -s {install_url} | {install_exec}'.format(**install_ctxt))
 
-#TODO Switch to secure installer
 #TODO output network id to status when connected to network
-    # cmd_install = ('curl -s 'https://raw.githubusercontent.com/zerotier/download.zerotier.com/master/htdocs/contact%40zerotier.com.gpg' '
-    #                 '| gpg --import && if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi')
 
     subprocess.check_call(cmd_install, shell=True)
 
     set_flag('zerotier.installed')
-    status_set('maintenance', 'Zerotier installed, ZT address ')
+    zt_address = subprocess.check_output('zerotier-cli -j info')
     
-# @when_all('zerotier.installed', 'zerotier-network-id')
+    status_set('maintenance', 'Zerotier installed, ZT address ')
+    application_version_set(get_upstream_version('zerotier-one'))
+
 @when('zerotier.installed')
 @when_not ('zerotier.network.joined')
 def zt_join_network():
