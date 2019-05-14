b764b1246828201692d512869617df0119dd66f8
seffyroff <seffyroff@gmail.com>
2019-05-14 07:20:26 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-juju destroy-model zt9 -y
-juju add-model zt9
-juju deploy cs:~jameinel/ubuntu-lite-7 --series bionic
-charm build
-charm push ~/charms/builds/zerotier/
-juju deploy cs:~seffyroff/zerotier-30
-juju config zerotier zerotier-network-id=e5cd7a9e1c39e9d3
-juju add-relation zerotier ubuntu-lite
