afea213d942e5a0de3365edfba3838229ae1e7c5
Phil Merricks <seffyroff@gmail.com>
2019-05-14 22:30:47 -0700

    updating excludes

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ juju add-model zt9
 juju deploy cs:~jameinel/ubuntu-lite-7 --series bionic
 charm build
 charm push ~/charms/builds/zerotier/
-juju deploy cs:~seffyroff/zerotier-30
+juju deploy cs:~seffyroff/zerotier-32
 juju config zerotier zerotier-network-id=e5cd7a9e1c39e9d3
 juju add-relation zerotier ubuntu-lite
